2b15a6e06070cd80bcbab5158d6015137e7e89b0
ranaharoon3222 <ranaharoon3222@gmail.com>
2021-11-28 08:47:13 +0500

    first commit

---
+++ src/vue/components/renderless/add-to-cart.vue
@@ -0,0 +1,19 @@
+
+
+<script>
+export default {
+  setup() {
+    // let cartForm = document.querySelector('form[action="/cart/add"]');
+    let mme = "mme";
+    // cartForm.addEventListener("submit", processCartForm);
+
+    const processCartForm = (e) => {
+      e.preventDefault();
+      console.log("object");
+    };
+
+    return { processCartForm, mme };
+  },
+};
+</script>
+
---
+++ src/vue/components/renderless/product-variant-selector.vue
@@ -0,0 +1,96 @@
+<script>
+import { toRefs, ref } from "vue";
+
+export default {
+  props: {
+    currentVariant: {
+      type: Object,
+      required: true,
+    },
+    productOptions: {
+      type: Object,
+      required: true,
+    },
+    productVariants: {
+      type: Object,
+      required: true,
+    },
+  },
+  setup(props, { slots }) {
+    const { currentVariant, productOptions, productVariants } = toRefs(props);
+
+    const selectedVariant = ref(currentVariant.value);
+
+    const selectOption = ({ name }, $event) => {
+      const selectedValue = $event.target.value;
+      const selectedOption = productOptions.value.find(
+        (option) => option.name === name
+      );
+      const allOptionValuesWithoutSelected = selectedOption.values.filter(
+        (value) => value !== selectedValue
+      );
+
+      const targetOptions = [
+        selectedValue,
+        ...selectedVariant.value.options.filter(
+          (value) => !allOptionValuesWithoutSelected.includes(value)
+        ),
+      ];
+
+      productVariants.value.some((variant) => {
+        if (variant.options.every((option) => targetOptions.includes(option))) {
+          selectedVariant.value = variant;
+
+          goToVariant(selectedVariant.value.id);
+          return true;
+        }
+      });
+    };
+
+    const goToVariant = function (value) {
+      var newurl =
+        window.location.protocol +
+        "//" +
+        window.location.host +
+        window.location.pathname +
+        `?variant=${value}`;
+      window.history.pushState({ path: newurl }, "", newurl);
+    };
+
+    //**********
+    // add To Cart
+    // *********
+
+    const processCartForm = (e) => {
+      e.preventDefault();
+      const formdata = new FormData(e.target);
+
+      fetch("/cart/add.js", {
+        method: "POST",
+        body: formdata,
+      })
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          console.log(data);
+          return data;
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    };
+
+    //**********
+    // End add To Cart
+    // *********
+
+    return () =>
+      slots.default({
+        selectedVariant: selectedVariant.value,
+        selectOption,
+        processCartForm,
+      });
+  },
+};
+</script>
